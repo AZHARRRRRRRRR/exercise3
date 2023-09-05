a512cd34646d045a01dc3854177edf754b4188d6
AZHARRRRRRRRR <143793259+AZHARRRRRRRRR@users.noreply.github.com>
2023-09-05 12:53:13 +0800

    azhar

---
+++ main.py
@@ -0,0 +1,10 @@
+def print_hi():
+    pelajar = {"nama": "Azhar",
+               "nomatriks": "18DDT21F2019"
+               }
+    print(pelajar["nama"])
+    print(pelajar["nomatriks"])
+
+
+if __name__ == '__main__':
+    print_hi()
